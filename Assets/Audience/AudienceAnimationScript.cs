af774666d5246d2bb80e21a03b4a296d4e01df11
mrrjam015 <mrrjam015@myuct.ac.za>
2017-08-26 13:52:41 +0200

    WorkingOnCrowdSpawningAndFaceing

--- Assets/Audience/AudienceAnimationScript.cs
+++ Assets/Audience/AudienceAnimationScript.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class AudienceAnimationScript : MonoBehaviour {
 
+	public Transform PlaceToLookAt;
+
 	Animator anim;
 
 	int cheer1Hash= Animator.StringToHash("Cheer1");
@@ -14,29 +16,31 @@ public class AudienceAnimationScript : MonoBehaviour {
 
 	void Start () {
 		anim = GetComponent<Animator> ();
+		transform.LookAt (PlaceToLookAt,Vector3.up);
+		transform.eulerAngles = new Vector3 (0, transform.eulerAngles.y, transform.eulerAngles.z);
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		if (Input.GetKeyDown(KeyCode.Y)) {
 			anim.SetTrigger(cheer1Hash);
-		//	Debug.Log("Animation called");
+			Debug.Log("Animation called");
 		}
 			
 		else if (Input.GetKeyDown(KeyCode.U)) {
 				anim.SetTrigger(cheer2Hash);
-		
+			Debug.Log("Animation called");
 		}else if (Input.GetKeyDown(KeyCode.I)) {
 			anim.SetTrigger(cheer3Hash);
-
+			Debug.Log("Animation called");
 		}
 		else if (Input.GetKeyDown(KeyCode.O)) {
 			anim.SetTrigger(cheer4Hash);
-
+			Debug.Log("Animation called");
 		}
 		else if (Input.GetKeyDown(KeyCode.P)) {
 			anim.SetTrigger(cheer5Hash);
-
+			Debug.Log("Animation called");
 		}
 
 	}
---
+++ Assets/CrowdSpawnScript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CrowdSpawnScript : MonoBehaviour {
+
+	public Transform[] transformArray;
+	public GameObject[] AuddienceToSpawn;
+	public Transform PlaceToLookAt;
+	// Use this for initialization
+	void Start () {
+		GameObject CroudMember = Instantiate (AuddienceToSpawn [0], transformArray [0].position, transformArray [0].rotation);
+		CroudMember.GetComponent<AudienceAnimationScript> ().PlaceToLookAt = PlaceToLookAt;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
